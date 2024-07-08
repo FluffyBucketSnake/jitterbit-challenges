5c0142b8009f0234c713cf26e7707d4bfa7c2e81
Bruno Henrique 'FluffyBucketSnake' Glowaski Morais <fluffybucketsnake@outlook.com>
2024-07-07 21:13:16 -0400

    feat: remap input fields

--- challenge-1/controllers/order.js
+++ challenge-1/controllers/order.js
@@ -1,17 +1,28 @@
 import { z } from "zod";
 
-const orderInSchema = z.object({
-  numeroPedido: z.string(),
-  valorTotal: z.number().int(),
-  dataCriacao: z.coerce.date(),
-  items: z.array(
-    z.object({
-      idItem: z.coerce.number(),
-      quantidadeItem: z.number().int(),
-      valorItem: z.number().int(),
-    }),
-  ),
-});
+const orderInSchema = z
+  .object({
+    numeroPedido: z.string(),
+    valorTotal: z.number().int(),
+    dataCriacao: z.coerce.date(),
+    items: z.array(
+      z.object({
+        idItem: z.coerce.number(),
+        quantidadeItem: z.number().int(),
+        valorItem: z.number().int(),
+      }),
+    ),
+  })
+  .transform(({ items, valorTotal, dataCriacao, numeroPedido }) => ({
+    orderId: numeroPedido,
+    value: valorTotal,
+    creationDate: dataCriacao,
+    items: items.map(({ valorItem, quantidadeItem, idItem }) => ({
+      productId: idItem,
+      quantity: quantidadeItem,
+      price: valorItem,
+    })),
+  }));
 
 export async function postOrder(request, response) {
   const parsedBody = orderInSchema.parse(request.body);
